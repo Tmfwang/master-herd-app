a6d44eef8f1656d69cc7b1f28766bbbd2ffd0110
Tor Martin Wang <tmwang@stud.ntnu.no>
2022-01-25 15:39:55 +0100

    På API-et kan man nå opprette brukere, og få en authentication token ved å sende inn korrekt brukernavn og passord

--- backend/fordypningsprosjektAPI/settings.py
+++ backend/fordypningsprosjektAPI/settings.py
@@ -27,6 +27,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'authentication.User'
+
 
 # Application definition
 
@@ -38,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+
+    # Additional apps
+    'rest_framework.authtoken',
+    'authentication'
 ]
 
 MIDDLEWARE = [
